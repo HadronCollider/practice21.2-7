f26bb16c132d259b0ff629c601bd2f8ea36e6c35
dashakoryakovskaya <dasha_cat27@mail.ru>
2021-06-20 00:30:16 +0300

    SettingActivity: selection type of quize and show what type do you select 2 type: classic (10 questions) and 3 wrong answers MarkActivity: right answers / tries

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/CountryByFlagQuizActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/CountryByFlagQuizActivity.kt
@@ -15,10 +15,14 @@ import com.opencsv.CSVReaderBuilder
 import kotlinx.android.synthetic.main.activity_country_by_flag_quiz.*
 import java.io.File
 import kotlin.random.Random
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
 
 class CountryByFlagQuizActivity : AppCompatActivity() {
 
     var points: Int = 0
+    var incorrect: Int = 0
     var tries: Int = 0
     var right_option = Random.nextInt(0, 3)
 
@@ -49,12 +53,14 @@ class CountryByFlagQuizActivity : AppCompatActivity() {
                     points++
                     Toast.makeText(this, "Правильно!", Toast.LENGTH_SHORT).show()
                 }
+                else incorrect++
 
                 //right_ans_tv.text = "Правильные ответы: $points / $tries"
-                if (tries == 10) {
+                if (((getSharedPreferences("settings", Context.MODE_PRIVATE).getInt("limitations", 0) == 0 || getSharedPreferences("settings", Context.MODE_PRIVATE).getInt("limitations", 0) == 1) && tries == 10) || (getSharedPreferences("settings", Context.MODE_PRIVATE).getInt("limitations", 0) == 3 && incorrect == 3)) {
                     val intent = Intent(this,MarkActivity::class.java)
                     Log.d("migav", points.toString())
                     intent.putExtra("points", points.toString())
+                    intent.putExtra("tries", tries.toString())
                     startActivity(intent)
 
                 } else {
@@ -69,12 +75,5 @@ class CountryByFlagQuizActivity : AppCompatActivity() {
                 }
             }
         }
-
-
-
-
-
-
-
     }
 }
--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingActivity.kt
@@ -4,16 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioGroup
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import android.view.View
 
 class SettingActivity : AppCompatActivity() {
+    var radiobn: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide();
         setContentView(R.layout.activity_setting)
 
+        val radioGroup = findViewById<RadioGroup>(R.id.radiogroup)
+
+        radiobn = getSharedPreferences("settings", Context.MODE_PRIVATE).getInt("limitations", 0)
+
+        when (radiobn){
+            0 -> radioGroup.check(R.id.radioclassic)
+            1 -> radioGroup.check(R.id.radioclassic)
+            2 -> radioGroup.check(R.id.radiotime)
+            3 -> radioGroup.check(R.id.radio3)
+        }
+
+        radioGroup?.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radioclassic -> radiobn = 1
+                R.id.radiotime -> radiobn = 2
+                R.id.radio3 -> radiobn = 3
+            }
+        }
+
+
         findViewById<Button>(R.id.buttondone).setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            val pref = getSharedPreferences("settings", Context.MODE_PRIVATE)
+            val editor = pref.edit()
+
+            editor.putInt("limitations", radiobn)
+            editor.apply()
             startActivity(intent)
         }
     }
-}
+
+    }
