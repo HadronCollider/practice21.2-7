0787a77d367579b47f2db28b2562754a084f4707
migavrilov <migav@icloud.com>
2021-06-25 10:00:41 +0300

    almost main activity_main.xml

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide();
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.buttonsettings).setOnClickListener {
